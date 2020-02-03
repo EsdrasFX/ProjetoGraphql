8c2b2e71d5c1780ff6b34c2e06017d6d7445ac66
esdras xavier <esdras.hc@gmail.com>
2020-02-02 21:40:58 -0300

    Aula 10

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -13,9 +13,9 @@ var books = [
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields:() => ({
-        id = {type: GraphQLString},
-        name = {type: GraphQLString},
-        genre = {type: GraphQLString}
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString}
     })
 });
 
